c40cba828a618c1094a90b67a6957d42cd3d4523
arizona2014 <andy.lisac@gmail.com>
2017-09-24 18:17:29 +0200

    Added solution

--- javascript1.js
+++ javascript1.js
@@ -3,3 +3,20 @@ The vowel subsequences in the word codewarriors are o,e,a,io. The longest of the
 
 Good luck!
 
+Solution:
+
+function solve(s){
+ var vowels = 'aeiou';
+ max = 0;
+ for(i=0;i<s.length;i++){
+   if(vowels.indexOf(s[i]) >= 0){
+    subsequence++
+    if(subsequence>max){
+      max = subsequence;
+    }
+   } else {
+    subsequence = 0;
+   }
+ }
+ return max;
+}
